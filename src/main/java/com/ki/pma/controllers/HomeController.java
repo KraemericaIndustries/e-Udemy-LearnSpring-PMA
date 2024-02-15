c5bca6ada3bb246827e7d9365521562e8a6e59d0
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-02-15 09:43:37 -0500

    Completed code through lecture 30: "Custom Query Continued: Project Status Query"

--- src/main/java/com/ki/pma/controllers/HomeController.java
+++ src/main/java/com/ki/pma/controllers/HomeController.java
@@ -1,16 +1,20 @@
 package com.ki.pma.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ki.pma.dao.EmployeeRepository;
 import com.ki.pma.dao.ProjectRepository;
+import com.ki.pma.dto.ChartData;
 import com.ki.pma.dto.EmployeeProject;
-import com.ki.pma.entities.Employee;
 import com.ki.pma.entities.Project;
 
 @Controller
@@ -23,11 +27,23 @@ public class HomeController {
 	EmployeeRepository empRepo;
 	
 	@GetMapping("/")
-	public String displayHome(Model model) {
+	public String displayHome(Model model) throws JsonProcessingException {
+
+		Map<String, Object> map = new HashMap<>();
+		
 		//  Query the database for projects
 		List<Project> projects =  proRepo.findAll();
 		model.addAttribute("projectsList", projects);
 
+		List<ChartData> projectData = proRepo.getProjectStatus();
+		
+		//  Let's convert projectData object into a json structure for use in Javascript
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(projectData);
+		//  [["NOTSTARTED", 1], ["INPROGRESS", 2], ["COMPLETED", 1]]
+		
+		model.addAttribute("projectStatusCnt", jsonString);
+		
 		//  Query the database for employees		
 		List<EmployeeProject> employeesProjectCnt =  empRepo.employeeProjects();
 		model.addAttribute("employeesListProjectsCnt", employeesProjectCnt);
---
+++ src/main/java/com/ki/pma/dto/ChartData.java
@@ -0,0 +1,8 @@
+package com.ki.pma.dto;
+
+public interface ChartData {
+	
+	public String getLabel();
+	public long getValue();
+
+}
