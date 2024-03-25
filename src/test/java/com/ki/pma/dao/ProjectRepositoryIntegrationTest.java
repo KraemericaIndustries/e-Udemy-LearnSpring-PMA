53d35b6c269d686e921e6d54eb873d6fb25ee830
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-03-25 11:10:38 -0400

    Completed code through lecture 41: "Using @SpringBootTest Annotation Correctly"

--- src/test/java/com/ki/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/ki/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,25 +1,22 @@
-package com.ki.dao;
+package com.ki.pma.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.ki.pma.ProjectManagementApplication;
 import com.ki.pma.dao.ProjectRepository;
 import com.ki.pma.entities.Project;
 
 //  https://stackoverflow.com/questions/52761758/how-i-can-resolve-this-problem-classspringjunit4classrunner-cannot-be-resolve
-@ContextConfiguration(classes=ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @SqlGroup({@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts= {"classpath:schema.sql", "classpath:data.sql"}),
 			@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts= "classpath:drop.sql")})
 public class ProjectRepositoryIntegrationTest {
